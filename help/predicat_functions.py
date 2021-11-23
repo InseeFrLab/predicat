e738a737a21ece06b2841418d57aa8e40312c685
dohaoq <yves-laurent.benichou@insee.fr>
2021-11-23 15:09:43 +0000

    clean code and change url

--- help/predicat_functions.py
+++ help/predicat_functions.py
@@ -5,38 +5,39 @@ Class and methods to easily call the API.
 
 Created on Mon Jan 25 13:51:07 2021
 
-@author: jeremylhour
+@author: jeremylhour&byl
 """
 
 import requests
-
+url='https://api.lab.sspcloud.fr/predicat/' # change with url of the service API
 
 class predicat:
     """
     predicat:
         class to call the PREDICAT API.
     """
-    def __init__(self, k=1, verbose=False):
+    def __init__(self):
         """
         init with a requests.session()
         
-        @param k (int): number of categories to predict
-        @param verbose (Bool): return the label of the category also?
+        
         """
         self.session = requests.session()
-        self.url = 'https://api-predicat.kub.sspcloud.fr/label?k='+str(k)+'&v='+str(verbose*1)
+        self.url = url
         
-    def predict(self, libelle, trace=False):
+    def predict(self, libelle, k=1, verbose=False, trace=False):
         """
         predict:
             call the API to predict the items in libelle
-            
+
+        @param k (int): number of categories to predict
+        @param verbose (Bool): return also the label of the predicted category
         @param libelle (list of str): list of items to predict 
         """
         if type(libelle) == str:
             libelle = [libelle]
         
-        call_url = self.url+'&q='+'&q='.join(libelle)
+        call_url = self.url+'label?k='+str(k)+'&v='+str(verbose*1)+'&q='+'&q='.join(libelle)
         response = self.session.get(call_url)
         
         if response.ok and trace:
@@ -46,15 +47,15 @@ class predicat:
     def decode(self, categorie, trace=False):
         """
         decode:
-            returns the title of the NA2008 category
+            returns the label of a category
 
         @param categorie (list): categories to decode
         """
         if type(categorie) == str:
             categorie = [categorie]
             
-        na2008_url = 'https://api-predicat.kub.sspcloud.fr/na2008?q='+'&q='.join(categorie)
-        response = self.session.get(na2008_url)
+        call_url = self.url+'label_description?q='+'&q='.join(categorie)
+        response = self.session.get(call_url)
         if response.ok and trace:
             print('Response received.')
         return response.json()
@@ -63,5 +64,5 @@ class predicat:
 # Example
 classifier = predicat()
 
-classifier.predict('SAVON DE MARSEILLE')
-classifier.decode('C20B')
+print(classifier.predict(['SAVON DE MARSEILLE',"Jus d'orange 1 litre"],verbose=True))
+print(classifier.decode('05.6.1.1.1'))
